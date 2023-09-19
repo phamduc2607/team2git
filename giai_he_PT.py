a75f1d1fea2663d066b96ebfe9cb3ddf618b6545
Trinh Bao Kien <pikachukien2812@gmail.com>
2023-09-19 14:01:54 +0700

    mahiru

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -1,14 +1,41 @@
-#bài này giải hệ phương trình x+2y=5 và 3x+4y =6
-# Yêu cầu hoàn chỉnh lại đoạn code
-#để có 1 app giải hệ phương trình có n phương trình n ẩn
 import numpy as np
-A = np.array([(1,2),(3,4)])
-B = np.array([5,6])
-A1  = np.linalg.inv(A) # tạo ma trận nghich đảo
-print(A)
-print(B)
-print(A1)
-X = np.dot(A1,B)
-print('Nghiem cua he:',X)
-print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-print('bbbbbbbbbbbbbbbbbbbbbbb')
+
+def solve_linear_equations(coefficients, constants):
+    try:
+        A = np.array(coefficients)
+        B = np.array(constants)
+        A_inv = np.linalg.inv(A)
+        X = np.dot(A_inv, B)
+        return X
+    except np.linalg.LinAlgError:
+        return None
+
+def main():
+    num_equations = int(input("Nhập số lượng phương trình: "))
+    num_unknowns = int(input("Nhập số lượng ẩn: "))
+
+    coefficients = []
+    constants = []
+
+    print("Nhập hệ số các phương trình:")
+    for i in range(num_equations):
+        equation = []
+        for j in range(num_unknowns):
+            coefficient = float(input(f"Nhập hệ số của ẩn x{j+1} trong phương trình {i+1}: "))
+            equation.append(coefficient)
+        coefficients.append(equation)
+
+        constant = float(input(f"Nhập hệ số tự do trong phương trình {i+1}: "))
+        constants.append(constant)
+
+    solution = solve_linear_equations(coefficients, constants)
+
+    if solution is not None:
+        print("Nghiệm của hệ phương trình:")
+        for i, value in enumerate(solution):
+            print(f"x{i+1} = {value}")
+    else:
+        print("Hệ phương trình vô nghiệm hoặc vô số nghiệm.")
+
+if __name__ == "__main__":
+    main()
