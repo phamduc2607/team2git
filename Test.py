925c5714bfc18fe83e317b936b8e9d76c155dc6c
phanvankien <pvkien30102002@gmail.com>
2023-09-19 14:24:54 +0700

    Tao giao dien tkinter

--- Test.py
+++ Test.py
@@ -1,59 +1,89 @@
+import tkinter as tk
+
 import numpy as np
 
-# k tao ma tran
-A = np.zeros((2, 2))
-B = np.zeros(2)
 
-print("Nhap a")
-k=0;
-for i in range(2):
-    for j in range(2):
-        k+=1;
-        A[i][j] = float(input(f"A[{k}] = "))
-print("Nhap B:")
-for i in range(2):
-    B[i] = float(input(f"B{i+1} = "))
+def solve_equation():
+    # Lấy giá trị n và m từ ô nhập liệu
+    n = int(num_eqn_entry.get())
+    m = int(num_vars_entry.get())
 
-# giai he pt
-A_inv = np.linalg.inv(A)
-X = np.dot(A_inv, B)
+    # Tạo cửa sổ giao diện cho việc nhập ma trận A và vector B
+    input_window = tk.Toplevel(root)
+    input_window.title("Nhập Ma Trận A và Vector B")
 
-print('Nghiem la:')
-for i in range(2):
-    # ket qua lam tron den chu so thap phan thu 2
-    print(f'x{i+1} = {X[i]:.2f}')
-import numpy as np
+    # Tạo ma trận A và vector B
+    A = np.zeros((n, m))
+    B = np.zeros(n)
 
-# Nhập số lượng phương trình và ẩn từ người dùng
-n = int(input("Nhập số lượng phương trình: "))
-m = int(input("Nhập số lượng ẩn: "))
+    def submit_input():
+        # Nhập ma trận A và vector B từ các ô nhập liệu
+        for i in range(n):
+            for j in range(m):
+                A[i][j] = float(matrix_entries[i][j].get())
 
-# k tao ma tran
-A = np.zeros((n, m))
-B = np.zeros(n)
+        for i in range(n):
+            B[i] = float(vector_entries[i].get())
 
-print("Nhap A:")
-k = 0
-for i in range(n):
-    for j in range(m):
-        k += 1
-        A[i][j] = float(input(f"A[{k}] = "))
+        # Giải hệ phương trình
+        augmented_matrix = np.column_stack((A, B))
+        rank_A = np.linalg.matrix_rank(A)
+        rank_augmented = np.linalg.matrix_rank(augmented_matrix)
 
-print("Nhap B:")
-for i in range(n):
-    B[i] = float(input(f"B[{i + 1}] = "))
+        if rank_A == rank_augmented == m:
+            X = np.linalg.solve(A, B)
+            result_label.config(text='Nghiệm của hệ phương trình:')
+            result_text = []
+            for i in range(m):
+                result_text.append(f'x{i + 1} = {X[i]:.2f}')
+            result_label2.config(text='\n'.join(result_text))
+        elif rank_A == rank_augmented < m:
+            result_label.config(text='Vô số nghiệm.')
+        else:
+            result_label.config(text='Vô nghiệm.')
 
-# Kiểm tra rank của ma trận A và ma trận mở rộng [A|B]
-augmented_matrix = np.column_stack((A, B))
-rank_A = np.linalg.matrix_rank(A)
-rank_augmented = np.linalg.matrix_rank(augmented_matrix)
+    # Tạo giao diện cho việc nhập ma trận A và vector B
+    matrix_entries = []
+    vector_entries = []
 
-if rank_A == rank_augmented == m:
-    X = np.linalg.solve(A, B)
-    print('Nghiem cua he phuong trinh: ')
-    for i in range(m):
-        print(f'x{i + 1} = {X[i]:.2f}')
-elif rank_A == rank_augmented < m:
-    print('Vo so nghiem.')
-else:
-    print('vo nghiem')
+    for i in range(n):
+        row_entries = []
+        for j in range(m):
+            entry = tk.Entry(input_window)
+            entry.grid(row=i, column=j)
+            row_entries.append(entry)
+        matrix_entries.append(row_entries)
+
+    for i in range(n):
+        entry = tk.Entry(input_window)
+        entry.grid(row=i, column=m)
+        vector_entries.append(entry)
+
+    submit_button = tk.Button(input_window, text="Xác nhận", command=submit_input)
+    submit_button.grid(row=n, columnspan=m+1)
+
+# Tạo cửa sổ giao diện chính
+root = tk.Tk()
+root.title("Giải Hệ Phương Trình Tuyến Tính")
+
+# Nhập số lượng phương trình và số lượng ẩn
+num_eqn_label = tk.Label(root, text="Số lượng phương trình:")
+num_eqn_label.pack()
+num_eqn_entry = tk.Entry(root)
+num_eqn_entry.pack()
+
+num_vars_label = tk.Label(root, text="Số lượng ẩn:")
+num_vars_label.pack()
+num_vars_entry = tk.Entry(root)
+num_vars_entry.pack()
+
+solve_button = tk.Button(root, text="Giải Hệ Phương Trình", command=solve_equation)
+solve_button.pack()
+
+# Nhãn để hiển thị kết quả
+result_label = tk.Label(root, text="")
+result_label.pack()
+result_label2 = tk.Label(root, text="")
+result_label2.pack()
+
+root.mainloop()
