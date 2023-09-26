b7c2c3fc54b5e3c9d50ccb977c945868f7656f32
dohaiduong0110 <111473155+dohaiduong0110@users.noreply.github.com>
2023-09-26 15:23:21 +0700

    Update diemb2.py

--- diemb2.py
+++ diemb2.py
@@ -4,18 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_csv('diemPython.csv', index_col=0, header=0)
+
+
 in_data = array(df.iloc[:, :])
+
 
 # Extract the total number of students
 tongsv = in_data[:, 1]
 tongsvabc= in_data[:, [3,5, 7]]
 tongsvlonhonD= in_data[:,[3,4,5,6,7,8,9]]
-print(tongsvabc)
-
+# row_values = in_data[2, 3:9]
+# Extract the data from columns 3 to 9 for all rows
+sinhvienlonhondmoilop= in_data[:,11]
 # Calculate the sum of total students
 total_students = np.sum(tongsv)
 total_ABC = np.sum(tongsvabc)
 total_londonD=np.sum(tongsvlonhonD)
+total_sinhvienlonhonD=(sinhvienlonhondmoilop)
+# max_total = np.max(total_per_row)
+#thong ke sinh vien co diem D
+
+
 
 # Create a bar chart
 plt.bar("Total Students", total_students)
@@ -23,21 +32,21 @@ plt.bar("Total Students", total_students)
 # Add labels
 plt.xlabel("Category")
 plt.ylabel("Total Students")
-plt.title("Total Students in the Exam")
+plt.title("Du lieu diem cua sinh vien")
 # Create a bar chart
 plt.bar("Total ABC", total_ABC)
 
-# Add labels
-plt.xlabel("Category")
-plt.ylabel("Total Students")
-plt.title("Total Students in the Exam")
 plt.bar("Total >=D", total_londonD)
 
-# Add labels
-plt.xlabel("Category")
-plt.ylabel("Total Students")
-plt.title("Total Students in the Exam")
+
 
 # Show the plot
 plt.show()
+# Create a bar chart
+plt.bar("Total Students", total_sinhvienlonhonD)
 
+# Add labels
+plt.xlabel("Category")
+plt.ylabel("Total Students")
+plt.title("Du lieu diem cua sinh vien")
+plt.show()
